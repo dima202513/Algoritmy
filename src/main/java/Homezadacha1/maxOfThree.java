436f27fcaa6675b53b55b87554d93e07889f0358
dima202513 <dmitrijra@gmail.com>
2025-02-10 08:53:32 +0100

    Initial commit

--- src/main/java/Homezadacha1/maxOfThree.java
+++ src/main/java/Homezadacha1/maxOfThree.java
@@ -1,4 +1,27 @@
 package Homezadacha1;
 
+import java.util.Scanner;
+
 public class maxOfThree {
+    public class MaxOfThree {
+        public static void main(String[] args) {
+            Scanner scanner = new Scanner(System.in);
+            System.out.print("enter number: ");
+            int a = scanner.nextInt();
+
+            System.out.print("enter number: ");
+            int b = scanner.nextInt();
+
+            System.out.print("enter number: ");
+            int c = scanner.nextInt();
+            if (b > a) {
+                a = b;
+            }
+            if (c > a) {
+                a = c;
+            }
+            System.out.println("max number: " + a);
+
+        }
+    }
 }
